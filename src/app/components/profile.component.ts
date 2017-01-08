c672c5bd22df410ce880d8307dc6d871dae2b142
ragefuljoe <ragefuljoe@gmail.com>
2017-01-08 08:57:42 -0600

    init

---
+++ src/app/components/profile.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { GithubService } from '../services/github.service';
+
+@Component({
+    selector: 'profile',
+    templateUrl: 'profile.component.html'
+})
+
+export class ProfileComponent {
+    user:any[];
+    repos:any[];
+    
+    constructor(private _githubService:GithubService){
+        this._githubService.getUser().subscribe(user=>{
+            this.user = user;
+        });
+
+        this._githubService.getRepos().subscribe(repos=>{
+            this.repos = repos;
+        })
+    }
+}
---
+++ src/app/services/github.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class GithubService{
+    private username:string;
+    private client_id = '';
+    private client_secret = '';
+
+    constructor(private _http:Http){
+        console.log('Github Service Ready');
+        this.username = 'ragefuljoe';
+    }
+
+    getUser(){
+        return this._http.get('http://api.github.com/users/'+this.username)
+            .map( res => res.json());
+    }
+
+    getRepos(){
+        return this._http.get('http://api.github.com/users/'+this.username+'/repos')
+            .map( res => res.json());
+    }
+}
